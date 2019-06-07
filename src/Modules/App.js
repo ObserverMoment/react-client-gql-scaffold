fa4c057d98d3ba4ff807ba3b884d0d1235b5630f
ObserverMoment <bosonsoul@gmail.com>
2019-06-07 19:02:33 +0100

    Ready for apollo install

---
+++ src/Modules/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from '../StyledComponents/Card'
+
+function App () {
+  return (
+    <div>
+      <Card>App says hello</Card>
+    </div>
+  )
+}
+
+export default App
---
+++ src/StyledComponents/Card.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro'
+
+export const Card = styled.div`
+  background: red;
+  font-size: 2em;
+  width: 300px;
+  height: 400px;
+  box-shadow: 2px 2px 2px 2px rgba(100, 100, 100, 0.5);
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+`
